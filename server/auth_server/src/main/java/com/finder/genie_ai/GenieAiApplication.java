287828bf28eaffac6a589619db5ae59427d46e7d
HanTaeWoong <gksxodnd007@gmail.com>
2018-01-15 12:37:49 +0900

    create project for spring boot

---
+++ server/auth_server/src/main/java/com/finder/genie_ai/GenieAiApplication.java
@@ -0,0 +1,12 @@
+package com.finder.genie_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenieAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenieAiApplication.class, args);
+	}
+}
